d13dc08031e25956c46442740e2109be48e4c283
Daniel Henderson-Ede <daniel@hendersonede.com>
2023-12-21 16:23:36 -0600

    Add site manifest and favicons

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -2,7 +2,8 @@ module.exports = {
 	title: "Daniel Henderson-Ede",
 	url: "https://daniel.hendersonede.com/",
 	language: "en-US",
-	description: "Design System and Accessibility Designer.",
+	description:
+		"Senior Accessibility Designer at CVS Health, helping to create an inclusive design system and improve accessibility annotations. Views are my own.",
 	author: {
 		name: "Daniel Henderson-Ede",
 		email: "daniel@hendersonede.com",
