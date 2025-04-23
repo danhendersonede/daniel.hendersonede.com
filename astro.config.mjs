0598d0f7b2468ab39003b69885fe415fc25e82bc
Daniel Henderson-Ede <daniel@hendersonede.com>
2025-04-22 23:43:28 -0500

    Add Adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,17 @@ import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   site: 'https://daniel.hendersonede.com',
   trailingSlash: 'never',
   integrations: [mdx(), sitemap(), react()],
+
   redirects: {
     '/article': '/articles',
     '/tool': '/tools'
-  }
+  },
+
+  adapter: netlify()
 });
