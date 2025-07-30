a25ee0fcfbea9fb1b1682fbb905827edb07a254a
Daniel Henderson-Ede <daniel@hendersonede.com>
2025-07-30 08:35:16 -0500

    Reorganize component directory

--- src/components/client/RevealEmail/RevealEmail.tsx
+++ src/components/client/RevealEmail/RevealEmail.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import styles from './reveal-email.module.css';
+import styles from './RevealEmail.module.css';
 
 const RevealEmail = () => {
   const [isRevealed, setIsRevealed] = useState(false);
